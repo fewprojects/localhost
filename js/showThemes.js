176e86a6565226919d1b3b8e08201f6d43ddced8
Ankit Upadhyaye <ankit.upa007@gmail.com>
2022-02-24 23:10:43 +0530

    prebuilt themes added to see

---
+++ js/showThemes.js
@@ -0,0 +1,74 @@
+function defaultTheme(){
+    let links = document.getElementsByTagName("link");
+    for (let i in links){
+        let link = links[i];
+        let e = link.href.includes("themes");
+        if(e){
+          links[i].href = "themes"
+        }
+}
+}
+// DARK THEMES
+
+function darkHacker(){
+  let links = document.getElementsByTagName("link");
+    for (let i in links){
+        let link = links[i];
+        let e = link.href.includes("themes");
+        if(e){
+          links[i].href = "css/localhost/themes/dark/hacker.css"
+          
+        }
+    }
+}
+
+function darkWriter(){
+    let links = document.getElementsByTagName("link");
+      for (let i in links){
+          let link = links[i];
+          let e = link.href.includes("themes");
+          if(e){
+            links[i].href = "css/localhost/themes/dark/writer.css"
+            
+          }
+      }
+}
+
+function darkGameboy(){
+  let links = document.getElementsByTagName("link");
+    for (let i in links){
+        let link = links[i];
+        let e = link.href.includes("themes");
+        if(e){
+          links[i].href = "css/localhost/themes/dark/gameboy.css"
+          
+        }
+    }
+}
+
+
+// LIGHT THEMES
+
+function lightCoffee(){
+  let links = document.getElementsByTagName("link");
+    for (let i in links){
+        let link = links[i];
+        let e = link.href.includes("themes");
+        if(e){
+          links[i].href = "css/localhost/themes/light/coffee.css"
+          
+        }
+    }
+}
+
+function lightWriter(){
+  let links = document.getElementsByTagName("link");
+    for (let i in links){
+        let link = links[i];
+        let e = link.href.includes("themes");
+        if(e){
+          links[i].href = "css/localhost/themes/light/writer.css"
+          
+        }
+    }
+}
